2f13e093988ae5cf03c603f603cac213e8086f56
rohit <rohitkumars1992@gmail.com>
2022-04-07 09:37:01 +0530

    added changes to xml file

--- src/test/java/ClassThree.java
+++ src/test/java/ClassThree.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class ClassThree {
+import org.testng.annotations.Test;
+
+public class ClassThree {
+
+    @Test
+    public void ClassThreeTest1()
+    {
+        System.out.println("This is test1 from class three");
+    }
 }
