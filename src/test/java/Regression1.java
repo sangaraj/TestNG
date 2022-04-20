586f7111bb3db7474ea3364653122da0e42d8f77
rohit <rohitkumars1992@gmail.com>
2022-04-20 13:46:28 +0530

    run different xml files using profiling concept

---
+++ src/test/java/Regression1.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+public class Regression1 {
+
+    @Test
+    public void regression1_1()
+    {
+        System.out.println("regression1 - 1");
+    }
+
+    @Test
+    public void regression1_2()
+    {
+        System.out.println("regression1 - 2");
+    }
+
+    @Test
+    public void regression1_3()
+    {
+        System.out.println("regression1 - 3");
+    }
+}
